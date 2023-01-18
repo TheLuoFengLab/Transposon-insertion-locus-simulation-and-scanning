869cc8052c28105661d5d5a77bc183e4b9a82c97
aragornwubo <46752436+aragornwubo@users.noreply.github.com>
2023-01-18 13:27:41 +0800

    Create TE_OVERLAP_READ_SCANNING.sh

---
+++ TE_OVERLAP_READ_SCANNING.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+SAMP=$1
+TE=$2
+NAME=$3
+LEFT=$( awk -F '[:-]' '{print $2}' <<< "${TE}" )
+RIGHT=$( awk -F '[:-]' '{print $3}' <<< "${TE}" )
+samtools index ${SAMP}.TE_masked.srt.bam
+samtools view -q 1 -h ${SAMP}.TE_masked.srt.bam ${TE} | awk -v x=${LEFT} -v y=${RIGHT} '
+$17~/SA:Z:/ && ($4<x+150 || $4>y-150) { match($17,/SA:Z:([^,]*),([^,]*),([+-]),([^,]*),([^,]*),/,a);
+if ($17~/,[0-9]+S[0-9]+M,/ && a[5]>0) {print a[1]"\t"a[2]-5"\t"a[2]+5"\t"$1};
+if ($17~/,[0-9]+M[0-9]+S,/ && a[5]>0) {match($17,/,([0-9]+)M/,b); print a[1]"\t"a[2]+b[1]-5"\t"a[2]+b[1]+5"\t"$1};
+if ($17~/,[0-9]+S[0-9]+M[0-9]+S,/ && a[5]>0) {match($17,/,([0-9]+)S([0-9]+)M([0-9]+)S,/,b); 
+if (b[1]>b[3]) {print a[1]"\t"a[2]-5"\t"a[2]+5"\t"$1} else {print a[1]"\t"a[2]+b[2]-5"\t"a[2]+b[2]+5"\t"$1}}
+}' > ${SAMP}_${NAME}.bed
+
+bedtools sort -i ${SAMP}_${NAME}.bed | bedtools merge -d 100 -i - > ${SAMP}_${NAME}.merged.bed
