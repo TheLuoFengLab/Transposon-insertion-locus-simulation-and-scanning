ace67864058a71ad75bbdc64b827e3cb761e073f
aragornwubo <46752436+aragornwubo@users.noreply.github.com>
2023-11-19 22:25:54 +0800

    Update Detect_Active_TEs_in_SWO.sh

--- Detect_Active_TEs_in_SWO.sh
+++ Detect_Active_TEs_in_SWO.sh
@@ -16,35 +16,52 @@ done
 ### Step 2:  'for' loop - Extracting and merging deletion and insertion variants:
 for VAR in *.fasta.var.txt ; do
 # Iterates over all .fasta.var.txt files from previous step
-
   awk '$5==1 && $4-$3>50 && $4-$3<20000 && $11-$10<=10 {print $2"\t"$3"\t"$4}' $VAR > ${VAR%.fasta.var.txt}.DEL.bed
-  awk '$5==1 && $4-$3<=10 && $11-$10>50 && $11-$10<20000 {print $2"\t"$3"\t"$4}' $VAR > ${VAR%.fasta.var.txt}.INS.bed
+  awk '$5==1 && $4-$3<=20 && $11-$10>50 && $11-$10<20000 {print $2"\t"$3"\t"$4}' $VAR > ${VAR%.fasta.var.txt}.INS.bed
   # Extracts deletion (DEL) and insertion (INS) variants shorter than 20000 bp and saves them to respective bed files
-
-  cat ${VAR%.fasta.var.txt}.DEL.tsv ${VAR%.fasta.var.txt}.INS.tsv | bedtools sort -i - > ${VAR%.fasta.var.txt}.INDEL.bed
-  # Concatenates the deletion and insertion files into a single INDEL.bed file and sort according to the chromosomes and coordinates
 done
 
-cat *.INS.bed | bedtools sort -i - > merged.temp.INS.bed
-bedtools intersect -wo -f 0.95 -r -a merged.temp.INS.bed -b merged.temp.INS.bed | awk '$2!=$5 || $3!=$6 {print $1"\t"$2"\t"}' > merged.temp.INS.overlaps.bed
-awk '
-}' merged.temp.INS.overlaps.bed > merged.temp.INS.filtered_overlaps.bed
-# Concatenate INS from all assemblies and remove duplications with reciprocal 95% overlap
+cat *.DEL.bed | bedtools sort -i - > merged.DEL.temp.bed
+# Concatenates all .DEL.bed files and sorts them, preparing them for further processing
+bedtools intersect -wo -f 0.95 -r -a merged.DEL.temp.bed -b merged.DEL.temp.bed | awk '$2!=$5 || $3!=$6' > merged.temp.DEL.overlaps.bed
+# Find all overlaps with over 95% reciprocal overlap within merged.temp.DEL.bed. The awk part filters out self-overlaps (where an interval overlaps with itself).
+cut -f 1,2,3 merged.temp.DEL.overlaps.bed | uniq > DEL.duplicate.bed
+# Extract unique intervals that are involved in overlaps and stores them in duplicate.DEL.bed.
+sort merged.DEL.temp.bed > merged.DEL.temp.srt.bed
+sort DEL.duplicate.bed > DEL.duplicate.srt.bed
+comm -23 merged.DEL.temp.srt.bed DEL.duplicate.srt.bed > DEL.non-duplicate.bed
+# Separate non-duplicate intervals from the original file
+while [[ $( wc -l merged.temp.DEL.overlaps.bed | cut -d " " -f 1) -gt 0 ]]; do
+awk '$2<=$5 {if ($3>=$6) {print $1"\t"$2"\t"$3} else {print $1"\t"$2"\t"$6}}' merged.temp.DEL.overlaps.bed > temp_duplicate.bed
+bedtools intersect -wo -f 0.95 -r -a temp_duplicate.bed -b temp_duplicate.bed | awk '$2!=$5 || $3!=$6' > merged.temp.DEL.overlaps.bed
+done
+# iteratively merge overlapping regions. This loop continues as long as there are overlaps found in merged.temp.DEL.overlaps.bed. It processes these overlaps and 
+# writes them to temp_duplicate.bed, then recalculates overlaps in each iteration.
+cat non-duplicate.DEL.bed temp_duplicate.bed | bedtools sort -i - > ALL10.DEL.filtered_overlaps.bed
+# Concatenate DEL from all assemblies and remove duplications with reciprocal 95% overlap
 
-cat *.DEL.bed | bedtools sort -i - | bedtools merge -d 10 -i - > merged.temp.DEL.merged.bed
-# Concatenate DEL from all assemblies and merge those within 10 bp distances
-
-cat merged.temp.INS.filtered_overlaps.bed merged.temp.DEL.merged.bed | bedtools sort -i - > ALL10.INDEL.bed
-# Merge deduplicated INS and DEL beds into one file
+cat *.INS.bed | bedtools sort -i - | bedtools merge -d 10 -i - > ALL10.INS.merged.bed
+# Concatenate INSs from all assemblies and deduplicate those within 10 bp distances
 
 ### Step 3: 'for' loop - Intersection analysis with bedtools:
-for BED in [list of INDEL.bed files]; do
-  bedtools intersect -f 0.95 -F 0.95 -c -a ALL10.INDEL.bed -b $BED > ALL10.${BED}
+for BED in Csiv4.DEL.bed T78.asem.DEL.bed SF.asem.DEL.bed GCA_019144245.1.DEL.bed GCA_019144225.1.DEL.bed GCA_019144195.1.DEL.bed GCA_019144185.1.DEL.bed GCA_019144155.1.DEL.bed GCA_019143665.1.DEL.bed GCA_018104345.1.DEL.bed; do
+  bedtools intersect -f 0.9 -F 0.9 -c -a ALL10.DEL.filtered_overlaps.bed -b $BED > TEMP.${BED}
   # Uses bedtools intersect to find overlaps between the ALL10.INDEL.bed file and each BED file
 done
 
-### Step 4: 'for' loop - Creating a combined file:
-for BED in ALL10.*.INDEL.bed ; do 
-  cut -f 4 $BED | paste TEMP - > TEMP1 ; mv TEMP1 TEMP; 
+for BED in *.INS.bed ; do
+  bedtools intersect -F 0.99 -c -a ALL10.DEL.filtered_overlaps.bed -b $BED > TEMP.${BED}
 done
+  
+
+### Step 4: Creating combined files:
+echo -e "CHR\tSTART\tEND\t"$( ls TEMP.*.DEL.bed ) | sed "s/ /\t/g" > header.txt
+for BED in TEMP.*.DEL.bed ; do 
+  cut -f 4 $BED | paste TEMP - > TEMP1
+  mv TEMP1 TEMP; 
+done
+cp header.txt ALL10.DEL.tsv
+paste ALL10.DEL.filtered_overlaps.bed TEMP1 >> ALL10.DEL.tsv
 # Iterates over ALL10.*.INDEL.bed files, extracts the 4th field, and merges it into a temporary file
+
+
